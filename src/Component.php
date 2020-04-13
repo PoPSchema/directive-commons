aaf0d29ff7dcc89bc7a5c3976b46a1427d183342
Leo <losoviz@gmail.com>
2020-04-13 19:08:43 +0800

    Added declare(strict_types=1);

--- src/Component.php
+++ src/Component.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\BasicDirectives;
 
 use PoP\Root\Component\AbstractComponent;
--- src/DirectiveResolvers/UseDefaultValueIfNullDirectiveResolver.php
+++ src/DirectiveResolvers/UseDefaultValueIfNullDirectiveResolver.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\BasicDirectives\DirectiveResolvers;
 
 use PoP\ComponentModel\DirectiveResolvers\GlobalDirectiveResolverTrait;
